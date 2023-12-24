d09fdd419227d2047f4fe78b036e5f8502cd9e75
Mariya Polshkova <polshkovamamariya@gmail.com>
2023-12-24 15:40:41 +0300

    Changes in class category

--- src/category.py
+++ src/category.py
@@ -17,5 +17,22 @@ class Category:
         self.description = description
         self.products = products
 
+        Category.category_count += 1
+
+        empty_list = []
+        for product in products:
+            if product not in empty_list:
+                Category.unique_products += 1
+                empty_list.append(product)
 
 
+first_category = Category(
+    'Напитки',
+    'Здесь находится описание категории',
+    ['молоко', 'вода', 'вино', 'сок', 'молоко'])
+
+second_category = Category(
+    'Фрукты',
+    'Здесь находится описание категории',
+    ['яблоки', 'mandarin', 'apple', 'apple']
+)
