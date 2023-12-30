d41f6edb062811f980e17193628720298aefd74a
Mariya Polshkova <polshkovamamariya@gmail.com>
2023-12-30 23:48:38 +0300

    New atributes for class Product

--- src/product.py
+++ src/product.py
@@ -14,3 +14,34 @@ class Product:
         self.description = description
         self.price = price
         self.count = count
+
+    @staticmethod
+    def create_product() -> dict:
+        new_product = {'name': 'name_',
+                       'description': 'description_',
+                       'price': 1000.0,
+                       'count': 12}
+        return new_product
+
+    @property
+    def change_price(self) -> float:
+        '''
+        Геттер для атрибута цены price
+        '''
+        return self.price
+
+    @change_price.setter
+    def change_price(self, new_price: float):
+        '''
+        Сеттер для установки новой цены товара
+        '''
+
+        if self.price > new_price:
+            user_input = input('Если вы хотите изменить стоимость товара, то введите "y",'
+                               'Если вы не желаете изменять стоимость, введите "n"')
+            if user_input == 'y':
+                self.price = new_price
+        else:
+            self.price = new_price
+
+
