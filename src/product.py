7c836fd1344a47a90ee596420dd5ff0f18d26e73
Mariya Polshkova <polshkovamamariya@gmail.com>
2024-01-14 13:02:51 +0300

    Changes in add methodics

--- src/product.py
+++ src/product.py
@@ -1,53 +1,46 @@
-
 class Product:
-    '''
+    """
     Класс для определения свойств продукта
-    '''
-    def __init__(
-            self,
-            name: str,
-            description: str,
-            price: float,
-            count: int
-    ):
+    """
+
+    def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
         self.price = price
-        self.count = count
+        self.quantity = quantity
 
     @staticmethod
     def create_product() -> dict:
-        new_product = {'name': 'name_',
-                       'description': 'description_',
-                       'price': 1000.0,
-                       'count': 12}
+        new_product = {"name": "name_", "description": "description_", "price": 1000.0, "quantity": 12}
         return new_product
 
     @property
     def change_price(self) -> float:
-        '''
+        """
         Геттер для атрибута цены price
-        '''
+        """
         return self.price
 
     @change_price.setter
     def change_price(self, new_price: float):
-        '''
+        """
         Сеттер для установки новой цены товара
-        '''
+        """
 
         if self.price > new_price:
-            user_input = input('Если вы хотите изменить стоимость товара, то введите "y",'
-                               'Если вы не желаете изменять стоимость, введите "n"')
-            if user_input == 'y':
+            user_input = input(
+                'Если вы хотите изменить стоимость товара, то введите "y",'
+                'Если вы не желаете изменять стоимость, введите "n"'
+            )
+            if user_input == "y":
                 self.price = new_price
         else:
             self.price = new_price
 
     def __str__(self):
-        return (f'Название продукта: {self.name}, '
-                f'{self.price}.руб, '
-                f'Остаток: {self.count} шт.')
+        return f"Название продукта: {self.name}, " f"{self.price}.руб, " f"Остаток: {self.quantity} шт."
 
     def __add__(self, other):
-        return self.price * self.count + other.price * other.quantity
+        if isinstance(other, Product):
+            return self.price * self.quantity + other.price * other.quantity
+        raise TypeError('Сложение двух разных типов продуктов невозможно')
